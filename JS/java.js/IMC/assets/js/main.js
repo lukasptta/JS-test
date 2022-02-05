2a00e1b2a4a5f78088e5c20d165ea1ccc72d32d9
lucas pita <93659077+lukasptta@users.noreply.github.com>
2022-02-05 20:03:24 -0300

    commit

---
+++ JS/java.js/IMC/assets/js/main.js
@@ -0,0 +1,10 @@
+//capturar evento de submit do formulário
+
+const form = document.querySelector('#formulario')
+
+form.addEventListener('submit' , function (e) {
+    e.preventDefault();
+    console.log('Evento previnido.')
+});
+
+function setResultado (msg)
